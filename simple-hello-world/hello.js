267790490e10bdbe66bebb71bea17e817d06582e
Wu Huijie <hjwu85@gmail.com>
2019-06-30 16:48:55 +0800

    enhance hello world example with simple count state and css examples

--- simple-hello-world/hello.js
+++ simple-hello-world/hello.js
@@ -1,6 +1,9 @@
 class HelloWorld extends HTMLElement {
   constructor() {
     super();
+
+    this.count = 0;
+
     const shadowRoot = this.attachShadow({ mode: 'open' });
     shadowRoot.innerHTML = `
       <style>
@@ -8,16 +11,42 @@ class HelloWorld extends HTMLElement {
           background-color: lightgrey;
           padding: 10px 20px;
         }
+        
+        .content {
+          display: flex;
+          flex-flow: row nowrap;
+        }
+
+        .content p {
+          margin-left: 10px;
+        }
+
+        button {
+          font-size: 20px;
+        }
       </style>
       <div id="container">
-        <button>Click Me!</button>
+        <h1>Hello!</h1>
+        <div class="content">
+          <button>Click Me!</button>
+          <p>Count: ${this.count}</p>
+        </div>
       </div>
     `;
     this.container = this.shadowRoot.querySelector('#container');
+    this.button = this.container.querySelector('button');
+    this.countDisplay = this.container.querySelector('p');
+
+    this.handleClick = this.handleClick.bind(this);
   }
 
   connectedCallback() {
-    this.container.querySelector('button').onclick = () => console.log('clicked!');
+    this.button.addEventListener('click', this.handleClick);
+  }
+
+  handleClick() {
+    this.count += 1;
+    this.countDisplay.innerHTML = `Count: ${this.count}`;
   }
 }
 
