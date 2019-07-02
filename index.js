f921a292dc33dcb2170e7f9742bf6f1b93acca3e
Wu Huijie <hjwu85@gmail.com>
2019-07-02 16:13:35 +0800

    improve script to run demos

--- index.js
+++ index.js
@@ -5,37 +5,52 @@ const yargs = require('yargs');
 
 const availableDemos = {
   hello: {
-    desc: 'Run Simple Hello World Example',
+    desc: 'Run Simple Hello World Example - Part 1',
+    command: ['npx', ['http-server', './simple-hello-world']],
+  },
+  hello2: {
+    desc: 'Run Simple Hello World Example - Part 2',
     command: ['npx', ['http-server', './simple-hello-world']],
   },
 };
 
-const { argv } = yargs
-  .usage('Usage: npm start -- -d [demo]')
-  .describe('d', 'Load a demo')
-  .demandOption(['d'])
-  .describe('l', 'List available demos');
+// eslint-disable-next-line no-unused-expressions
+yargs
+  .command('*', 'default command', () => yargs.showHelp())
+  .command(
+    'demo <demo>',
+    'run demos',
+    () => {},
+    ({ demo }) => {
+      yargs.positional('demo', {
+        describe: 'Demo to run',
+        type: 'string',
+      });
 
-let childProcess;
+      let childProcess;
 
-if (availableDemos[argv.d] && availableDemos[argv.d].command) {
-  childProcess = spawn(...availableDemos[argv.d].command, { stdio: 'inherit' });
-}
+      if (availableDemos[demo] && availableDemos[demo].command) {
+        childProcess = spawn(...availableDemos[demo].command, { stdio: 'inherit' });
+      }
 
-if (childProcess) {
-  if (childProcess.stdout) {
-    childProcess.stdout.on('data', data => {
-      console.log(data.toString());
-    });
-  }
+      if (childProcess) {
+        if (childProcess.stdout) {
+          childProcess.stdout.on('data', data => {
+            console.log(data.toString());
+          });
+        }
 
-  if (childProcess.stderr) {
-    childProcess.stderr.on('data', data => {
-      console.log(`${data}`);
-    });
-  }
+        if (childProcess.stderr) {
+          childProcess.stderr.on('data', data => {
+            console.log(`${data}`);
+          });
+        }
 
-  childProcess.on('close', code => {
-    console.log(`child process exited with code ${code}`);
-  });
-}
+        childProcess.on('close', code => {
+          console.log(`child process exited with code ${code}`);
+        });
+      }
+    }
+  )
+  .command('ls', 'list all available demos', () => {}, () => console.log('demo command is ran'))
+  .demandCommand().argv;
